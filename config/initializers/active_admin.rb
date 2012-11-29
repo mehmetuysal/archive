b37bea5d3a212410ae6a878fa14871e126537244
Mehmet Uysal <mehmet@uysal.me>
2012-11-29 23:37:08 +0200

    deneme

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -7,7 +7,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Arşiv"
+  config.site_title = "Arsiv"
   config.default_per_page = 25
 
   # Set the link url for the title. For example, to take
